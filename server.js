526256ebf471e05d42bafa73caa77bd280400581
raptiq <raptiq@gmail.com>
2016-03-06 13:51:48 -0500

    initial commit, getting server serving

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var isProduction = process.env.NODE_ENV === 'production';
+var port = "8080";
+var publicPath = path.resolve(__dirname, 'public');
+
+// We point to our static assets
+app.use(express.static(publicPath));
+
+// And run the server
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
+});
