3bc85d9ad05c1b31c4c1eaf25d4aa292fe5e7f22
Jack Burns <jack@raptiq.com>
2016-03-06 20:13:51 -0500

    get serious

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const http = require('http');
+const express = require('express');
+const app = express();
 
-var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, 'public');
+app.use(require('morgan')('short'));
 
-// We point to our static assets
-app.use(express.static(publicPath));
+(function initWebpack() {
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack/common.config');
+  const compiler = webpack(webpackConfig);
 
-// And run the server
-app.listen(port, function () {
-  console.log('Server running on port ' + port);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true, publicPath: webpackConfig.output.publicPath,
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
+  }));
+
+  app.use(express.static(__dirname + '/'));
+})();
+
+app.get(/.*/, function root(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000, function onListen() {
+  const address = server.address();
+  console.log('Listening on: %j', address);
+  console.log(' -> that probably means: http://localhost:%d', address.port);
 });
